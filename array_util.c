c9c48ef120a0b3f9473470cee8fcd003e6af6c09
ashwinka <ashwinka@INashwinka.local>
2016-01-04 15:24:03 +0530

    [Ashwin]- Created areEqual function.
    What changes have you made?
    * Wrote test to check wheather the areEqual functionality works properly.
    
    * Added areEqual function to check wheather the arrays are equal
    by checking each value of the array.
    
    Why do we need this change?
    * Test is needed to verify the function.
    
    * Function which helps to check two arrays.
    
    	modified:   array_util.c
    	modified:   test
    	modified:   test_array_util.c

--- array_util.c
+++ array_util.c
@@ -1,10 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "array_util.h"
 
 ArrayUtil create(int type_size,int length){
   ArrayUtil arr_util;
-  arr_util.base =(void *)malloc(type_size);
+  arr_util.base =(void *)calloc(type_size,length);
   arr_util.length = length;
   arr_util.type_size = type_size;
   return arr_util;
+};
+
+int areEqual(ArrayUtil arr1,ArrayUtil arr2){
+  int size = arr1.length * arr1.type_size;
+  char *a;
+  char *b;
+  if(arr1.length == arr2.length && arr1.type_size == arr2.type_size){
+    while(size){
+      a = arr1.base;
+      b = arr2.base;
+      if(*a != *b){
+        return 0;
+      }
+      arr1.base++;
+      arr2.base++;
+      size--;
+    }
+    return 1;
+  }
+  return 0;
 };
--- test_array_util.c
+++ test_array_util.c
@@ -6,9 +6,34 @@ void test_create_returns_new_array_utils(){
   ArrayUtil arr_util =create(4,2);
   assert(arr_util.length ==2);
   assert(arr_util.type_size ==4);
+  printf("✓ test_create_returns_new_array_utils\n");
+};
+
+void test_areEqual_returns_1_when_both_are_equal(){
+  ArrayUtil arr1 = create(4,9);
+  ArrayUtil arr2 = create(4,9);
+  assert(areEqual(arr1,arr2)==1);
+  printf("✓ test_areEqual_returns_1_when_both_are_equal\n");
+};
+
+void test_areEqual_returns_0_when_their_type_size_is_different(){
+  ArrayUtil arr1 = create(8,3);
+  ArrayUtil arr2 = create(4,3);
+  assert(areEqual(arr1,arr2)==0);
+  printf("✓ test_areEqual_returns_0_when_their_type_size_is_different\n");
+};
+
+void test_areEqual_returns_0_when_their_length_is_different(){
+  ArrayUtil arr1 = create(4,4);
+  ArrayUtil arr2 = create(4,5);
+  assert(areEqual(arr1,arr2)==0);
+  printf("✓ test_areEqual_returns_0_when_their_length_is_different\n");
 };
 
 int main (void){
   test_create_returns_new_array_utils();
+  test_areEqual_returns_1_when_both_are_equal();
+  test_areEqual_returns_0_when_their_type_size_is_different();
+  test_areEqual_returns_0_when_their_length_is_different();
   return 0;
 };
